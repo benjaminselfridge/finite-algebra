ebec799113bfde4c3f4f18d288a08649e093071f
Ben Selfridge <benselfridge@galois.com>
2021-06-10 15:59:48 -0700

    Changes laws function slightly

--- src/Algebra/Finite/Class.hs
+++ src/Algebra/Finite/Class.hs
@@ -8,6 +8,7 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 
 import Data.Maybe (listToMaybe, mapMaybe)
+import Control.Arrow (second)
 
 checkLaws :: Set a -> [(law, Property a)] -> Maybe (law, [a])
 checkLaws d laws =
@@ -22,22 +23,22 @@ class Algebra alg a law | alg -> a, alg -> law where
   algebraSet :: alg -> Set a
 
   -- | The laws that must hold for the algebra.
-  algebraLaws :: alg -> [(law, Property a)]
+  algebraLaws :: [(law, alg -> Property a)]
 
 -- | Check all the laws hold for a given algebra. If a law does not hold, return
 -- @Just (law, inputs)@, where @law@ is the law that fails, and @inputs@ are the
 -- inputs to the law that constitute a counterexample.
 checkAlgebra :: Algebra alg a law => alg -> Maybe (law, [a])
-checkAlgebra alg = checkLaws (algebraSet alg) (algebraLaws alg)
+checkAlgebra alg = checkLaws (algebraSet alg) (second ($ alg) <$> algebraLaws)
 
 class (Algebra alg a law, Algebra alg' b law) => Morphism m morphismLaw alg alg' a b law | m -> alg, m -> alg', m -> morphismLaw where
   morphismDomain   :: m -> alg
   morphismCodomain :: m -> alg'
   morphism         :: m -> a -> b
-  morphismLaws     :: m -> [(morphismLaw, Property a)]
+  morphismLaws     :: [(morphismLaw, m -> Property a)]
 
 checkMorphism :: Morphism m morphismLaw alg alg' a b law => m -> Maybe (morphismLaw, [a])
-checkMorphism m = checkLaws (algebraSet (morphismDomain m)) (morphismLaws m)
+checkMorphism m = checkLaws (algebraSet (morphismDomain m)) (second ($ m) <$> morphismLaws)
 
 morphismTable :: Morphism m morphismLaw alg alg' a b law => m -> [(a, b)]
 morphismTable m = [ (a, morphism m a)
